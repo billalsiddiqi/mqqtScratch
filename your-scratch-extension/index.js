ee49372cf138aeee53944c6ca2e9165d4fba8a01
Khwaja Billal Siddiqi <52275107+billalsiddiqi@users.noreply.github.com>
2024-02-26 10:22:25 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -3,20 +3,11 @@ const ArgumentType = require('../../extension-support/argument-type');
 // const TargetType = require('../../extension-support/target-type');
 
 class Scratch3YourExtension {
-
+    
     constructor (runtime) {
         this.runtime = runtime;
         this.host = 'test.mosquitto.org';
-        this.port = 8081;
-        this.topic = '/scratchExtensionTopic';
-        this.useTLS = true;
-        this.username = null;
-        this.password = null;
-        this.state = { status: 1, msg: 'loaded' };
-        this.mqtt = null;
-        this.reconnectTimeout = 2000;
-        this.messagePayload = '';
-        this.newMessage = false;
+        this.messages = {};
     }
 
     /**
@@ -41,191 +32,60 @@ class Scratch3YourExtension {
             // your Scratch blocks
             blocks: [
                 {
-                    opcode: 'sendMessage',
+                    opcode: 'publish',
                     blockType: BlockType.COMMAND,
-                    text: 'send message %s to topic %s',
+                    text: 'publish [message] to [topic]',
                     arguments: {
-                        MESSAGE: {
+                        message: {
                             type: ArgumentType.STRING,
-                            defaultValue: 'message'
+                            defaultValue: 'Hello mqtt'
                         },
-                        TOPIC: {
+                        topic: {
                             type: ArgumentType.STRING,
-                            defaultValue: 'topic'
+                            defaultValue: 'scratch-extension'
+                        }
+                    }
+                },
+                {
+                    opcode: 'onMessage',
+                    blockType: BlockType.HAT,
+                    text: 'when new message on [topic]',
+                    arguments: {
+                        topic: {
+                            type: ArgumentType.STRING,
+                            defaultValue: 'scratch-extension'
                         }
                     }
                 },
                 {
                     opcode: 'getMessage',
                     blockType: BlockType.REPORTER,
-                    text: 'message'
-                },
-                {
-                    opcode: 'whenMessageArrived',
-                    blockType: BlockType.HAT,
-                    text: 'when message arrived'
-                },
-                {
-                    opcode: 'isMessageArrived',
-                    blockType: BlockType.BOOLEAN,
-                    text: 'message arrived'
-                },
-                {
-                    opcode: 'setTLS',
-                    blockType: BlockType.COMMAND,
-                    text: 'secure connection %m.secureConnection',
+                    text: 'latest message on [topic]',
                     arguments: {
-                        SECURE: {
+                        topic: {
                             type: ArgumentType.STRING,
-                            menu: 'secureConnection',
-                            defaultValue: 'true'
+                            defaultValue: 'scratch-extension'
                         }
                     }
-                },
-                {
-                    opcode: 'setHost',
-                    blockType: BlockType.COMMAND,
-                    text: 'Host %s',
-                    arguments: {
-                        HOST: {
-                            type: ArgumentType.STRING,
-                            defaultValue: 'test.mosquitto.org'
-                        }
-                    }
-                },
-                {
-                    opcode: 'setTopic',
-                    blockType: BlockType.COMMAND,
-                    text: 'Subscribe to topic %s',
-                    arguments: {
-                        TOPIC: {
-                            type: ArgumentType.STRING,
-                            defaultValue: '/scratchExtensionTopic'
-                        }
-                    }
-                },
-                {
-                    opcode: 'setPort',
-                    blockType: BlockType.COMMAND,
-                    text: 'Port %n',
-                    arguments: {
-                        PORT: {
-                            type: ArgumentType.STRING,
-                            defaultValue: '8081'
-                        }
-                    }
-                },
-                {
-                    opcode: 'connect',
-                    blockType: BlockType.COMMAND,
-                    text: 'connect'
                 }
-            ],
+            ]
         };
     }
 
-
-    /**
-     * implementation of the block with the opcode that matches this name
-     *  this will be called when the block is used
-     */
-    getMessage() {
-        return this.messagePayload;
+    publish(args) {
+        console.log(args)
     }
 
-    whenMessageArrived() {
-        return this.newMessage;
+    onMessage(args) {
+        // Check if a new message has been received on the specified topic
+        const topic = args.topic;
+        return this.messages.hasOwnProperty(topic);
     }
 
-    isMessageArrived() {
-        return this.newMessage;
-    }
-
-    setTLS(args) {
-        this.useTLS = args.SECURE === 'true';
-    }
-
-    setHost(args) {
-        this.host = args.HOST;
-    }
-
-    setTopic(args) {
-        this.topic = args.TOPIC;
-    }
-
-    setPort(args) {
-        this.port = args.PORT;
-    }
-
-    connect() {
-        this.MQTTconnect();
-    }
-
-    MQTTconnect() {
-        if (typeof path === 'undefined') {
-            path = '/mqtt';
-            console.log('path=' + path);
-        }
-
-        this.mqtt = new Paho.MQTT.Client(
-            this.host,
-            this.port,
-            'web_' + parseInt(Math.random() * 100, 10)
-        );
-
-        var options = {
-            timeout: 3,
-            useSSL: this.useTLS,
-            cleanSession: true,
-            onSuccess: this.onConnect.bind(this),
-            onFailure: this.onConnectionLost.bind(this)
-        };
-
-        this.mqtt.onConnectionLost = this.onConnectionLost.bind(this);
-        this.mqtt.onMessageArrived = this.onMessageArrived.bind(this);
-
-        if (this.username !== null) {
-            options.userName = this.username;
-            options.password = this.password;
-        }
-
-        console.log(
-            'Host=' +
-                this.host +
-                ', port=' +
-                this.port +
-                ', path=' +
-                path +
-                ' TLS = ' +
-                this.useTLS +
-                ' username=' +
-                this.username +
-                ' password=' +
-                this.password
-        );
-        this.mqtt.connect(options);
-    }
-
-    onConnect() {
-        console.log('trying to connect');
-        this.state = { status: 1, msg: 'connecting ...' };
-        console.log('Connected to ' + this.host + ':' + this.port + path);
-        this.mqtt.subscribe(this.topic, { qos: 0 });
-        this.state = { status: 2, msg: 'connected' };
-    }
-
-    onMessageArrived(message) {
-        console.log('message arrived ' + message.payloadString);
-        this.messagePayload = message.payloadString;
-        this.newMessage = true;
-    }
-
-    onConnectionLost(response) {
-        this.state = { status: 1, msg: 'connecting ...' };
-        setTimeout(this.MQTTconnect.bind(this), this.reconnectTimeout);
-        console.log(
-            'connection lost: ' + response.errorMessage + '. Reconnecting'
-        );
+    getMessage(args) {
+        // Return the latest message received on the specified topic
+        const topic = args.topic;
+        return this.messages[topic] || '';
     }
 }
 
